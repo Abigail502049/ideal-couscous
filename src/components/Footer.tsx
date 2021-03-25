e646705529bf74915eb9a4b75e73d1bf351166dc
Gabriel Saillard <gabriel@saillard.dev>
2021-03-25 17:54:50 +0100

    fix missing alt prop on footer logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import styles from './Footer.module.scss'
 
 export default function Footer() {
 	return <footer className={styles.footer}>
-		<img src="/logo.png"/>
+		<img src="/logo.png" alt="idealcouscous"/>
 		<span>Règles de Respect de la Vie Privée  Modalités relatives aux cookies  A propos de IdealCouscous  Condition générales d'abonnement  Aide  A propos d'IdéalCouscous</span>
 		<span>Publicités ciblées par centre d'intérêt  Gérer vos préférences</span>
 		<span>© IdéalCouscous. Tous droits réservés</span>
