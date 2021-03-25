257de37e6af5ddc98ba159f9b686ce3a6448dddc
Gabriel Saillard <gabriel@saillard.dev>
2021-03-25 14:55:26 +0100

    feat: basic movie search

--- src/App.js
+++ src/App.js
@@ -1,17 +1,63 @@
+import { useState, useEffect } from 'react'
+import queryTmdb from 'utils/queryTmdb'
 import Header from 'components/Header'
 import SearchInput from 'components/SearchInput'
 import styles from './App.module.scss'
 
 export default function App() {
-	return (
+	const [searchTerms, setSearchTerms] = useState(null)
+	const [searchResults, setSearchResults] = useState([])
+
+	useEffect(() => {
+		if (searchTerms === null) {
+			setSearchResults([])
+			return
+		}
+
+		const abortCtrl = new AbortController()
+		queryTmdb('/search/movie', [
+			['query', searchTerms],
+			['language', 'fr-FR'],
+			['region', 'FR']
+		], abortCtrl.signal).then(resp => {
+			setSearchResults(() => resp.results.slice(0, 5))
+		}).catch(() => {
+			// no-op
+		})
+
+		return () => {
+			abortCtrl.abort()
+		}
+	}, [searchTerms])
+
+	return <>
 		<div className={styles.root}>
 			<Header/>
 			<section className={styles.topPrompt}>
 				<h2>Trouvez le film idéal</h2>
 				<div className={styles.searchContainer}>
-					<SearchInput/>
+					<SearchInput
+						onChange={searchstr => {
+							setSearchTerms(searchstr)
+						}}
+						onSubmit={searchstr => {
+							alert(searchstr)
+						}}
+					/>
+				</div>
+				<div className={styles.searchResultsContainer}>
+					<div
+						className={styles.searchResults}
+						style={{
+							display: searchResults.length ? 'inherit' : 'hidden'
+						}}
+					>
+						{searchResults.map(item =>
+							<p key={item.id}>{ item.title }</p>
+						)}
+					</div>
 				</div>
 			</section>
 		</div>
-	)
+	</>
 }
