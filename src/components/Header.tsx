2cbf38124dc9054953dfdade61a4c875d3f1c8b7
Strum <phillipsromeo@gmail.com>
2021-03-26 12:19:30 +0100

    - Made font bigger in header - Fixed issue if in movie section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,9 @@ import Select from "./Select";
 export default function Header() {
 	function handleClick(el : string) {
 		// @ts-ignore
-		document.querySelector('.' + el).scrollIntoView({behavior: "smooth", block: "start"})
+		const element = document.querySelector('.' + el)
+		if (element !== null)
+			element.scrollIntoView({behavior: "smooth", block: "start"})
 	}
 
 	return <header className={styles.header}>
@@ -14,15 +16,21 @@ export default function Header() {
 			<Link to="/">
 				<h1>Idealcouscous.tv</h1>
 			</Link>
-			<button type="button" onClick={() => handleClick('latest')}>
-				Dernière Sorties
-			</button>
-			<button type="button" onClick={() => handleClick('recommended')}>
-				Recommandés
-			</button>
-			<button type="button" onClick={() => handleClick('best')}>
-				Meilleurs films
-			</button>
+			<Link to="/">
+				<button type="button" onClick={() => handleClick('recommended')}>
+					Recommandés
+				</button>
+			</Link>
+			<Link to="/">
+				<button type="button" onClick={() => handleClick('latest')}>
+					Dernière Sorties
+				</button>
+			</Link>
+			<Link to="/">
+				<button type="button" onClick={() => handleClick('best')}>
+					Meilleurs films
+				</button>
+			</Link>
 		</div>
 		<Select/>
 	</header>
